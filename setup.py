2248e11fb847167c3056c9fa1e3a4bfdb7247b45
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-02 16:07:44 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+  name='valeera',
+  version='0.1',
+  url='https://github.com/nikitanovosibirsk/valeera',
+  author='Nikita Tsvetkov',
+  author_email='nikitanovosibirsk@yandex.com',
+  license='MIT',
+  packages=['valeera']
+)
