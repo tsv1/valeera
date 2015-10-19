ca03587b09264ff36007cea817f0fcb6f62a4710
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-19 14:05:21 +0600

    add equality operator validation

--- valeera/__init__.py
+++ valeera/__init__.py
@@ -1,5 +1,9 @@
+from district42 import SchemaType
 from .abstract_validator import AbstractValidator
 from .abstract_formatter import AbstractFormatter
 from .validator import Validator
 from .formatter import Formatter
 from .errors import *
+
+
+SchemaType.__eq__ = lambda self, other: Validator(Formatter()).validate(other, self).passes()
