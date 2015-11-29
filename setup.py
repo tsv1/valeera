74a119b57ce13bc573aa4f6a8a585379183052f4
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-11-29 22:07:59 +0600

    bump version: 0.5.4 -> 0.5.5

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 setup(
   name='valeera',
   description='Validator for district42 schema',
-  version='0.5.4',
+  version='0.5.5',
   url='https://github.com/nikitanovosibirsk/valeera',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
   license='MIT',
   packages=find_packages(),
   install_requires=[
-    'district42==0.5.4',
-    'delorean==0.5.0'
+    'district42==0.5.5',
+    'Delorean==0.5.0'
   ]
 )
