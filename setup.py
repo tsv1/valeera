359ef77c56f22f81318b37a34a5bdb103e91b659
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2017-11-28 14:40:13 +0700

    bump version

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 setup(
   name='valeera',
   description='Validator for district42 schema',
-  version='0.5.5',
+  version='0.6.1',
   url='https://github.com/nikitanovosibirsk/valeera',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
   license='MIT',
   packages=find_packages(),
   install_requires=[
-    'district42==0.5.5',
+    'district42==0.6.1',
     'Delorean==0.5.0'
+  ],
+  dependency_links=[
+    'https://github.com/nikitanovosibirsk/district42/tarball/d39db770d53fd36cf62b31b55944aca719f4df71#egg=district42-0.6.1'
   ]
 )
